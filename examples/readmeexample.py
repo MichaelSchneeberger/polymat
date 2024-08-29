d7f78a578011a9030fa34edc6ea93071e35fddd6
Michael Schneeberger <michael.schneeberger@fhnw.ch>
2024-08-29 15:45:50 +0200

    Clean up readmeexample.py.

--- examples/readmeexample.py
+++ examples/readmeexample.py
@@ -1,46 +1,46 @@
 import polymat
 
-# create the state object
+# The state object is passed through all operations involved in a polynomial
+# expression
 state = polymat.init_state()
 
 
-# (scalar) polynomial matrix expression
-#######################################
+# polynomial expression
+#######################
 
+# Define the polynomial variables and stack them into one vector
 names = ('x1', 'x2')
 x1, x2 = (polymat.define_variable(n) for n in names)
 x = polymat.v_stack((x1, x2))
 
-f1 = x1 + x2
-f2 = x1 + x1*x2
+# Create a polynomial expression using arithmetic operations
+f = (x1 + x2) + (x1 + x1*x2)
 
-f3 = f1 + f2
-
-# prints a nicely printable string representation of the expression
+# Prints a pretty string representation of the polynomial expression:
 # add(add(x1, x2), add(x1, mul(x1, x2)))
-print(f'{f3}')
+print(f'{f}')
 
-# prints the string representation of the dataclass
+# Prints the Python implementation of the expression using dataclasses:
 # f3 = ExpressionImpl(
 #   child=AdditionExprImpl(
 #       left=AdditionExprImpl(
-#           left=FromVariableImpl(variable='x1', nvar=1),
-#           right=FromVariableImpl(variable='x2', nvar=1)),
+#           left=FromVariableImpl(variable='x1', size=1),
+#           right=FromVariableImpl(variable='x2', size=1)),
 #       right=AdditionExprImpl(
-#           left=FromVariableImpl(variable='x1', nvar=1),
+#           left=FromVariableImpl(variable='x1', size=1),
 #           right=ElementwiseMultImpl(
-#               left=FromVariableImpl(variable='x1', nvar=1),
-#               right=FromVariableImpl(variable='x2', nvar=1)))))
-print(f'{f3=}')
+#               left=FromVariableImpl(variable='x1', size=1),
+#               right=FromVariableImpl(variable='x2', size=1)))))
+print(f'{f=}')
 
 
 # sympy representation
 ######################
 
-# computes the sympy representation of the expression
-state, sympy_repr = polymat.to_sympy(f3,).apply(state)
+# Compute the sympy representation of the expression
+state, sympy_repr = polymat.to_sympy(f,).apply(state)
 
-# prints the sympy representation
+# Prints the sympy representation:
 # x1*x2 + 2.0*x1 + x2
 print(f'{sympy_repr}')
 
@@ -48,10 +48,10 @@ print(f'{sympy_repr}')
 # array representation
 ######################
 
-# computes the array representation of the expression
-state, array_repr = polymat.to_array(f3, x).apply(state)
+# Compute the array representation of the expression
+state, array_repr = polymat.to_array(f, x).apply(state)
 
-# prints the array representations
+# Prints the array representations:
 # array_repr.data[1]=array([[2., 1.]])
 print(f'{array_repr.data[1]=}')               # numpy array
 # array_repr.data[2].toarray()=array([[0. , 0.5, 0.5, 0. ]])
