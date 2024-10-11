c2e57a5e58a680ea38f463c9d869c7f8e69154df
Michael Schneeberger <michael.schneeberger@fhnw.ch>
2024-10-11 13:56:00 +0200

    Bug fix: Allow full support for slicing when using __getitem__ on expressions.

--- polymat/expressiontree/operations/getitem.py
+++ polymat/expressiontree/operations/getitem.py
@@ -31,19 +31,26 @@ class GetItem(SingleChildExpressionNode):
     def apply(self, state: State) -> tuple[State, SparseRepr]:
         state, child = self.child.apply(state=state)
 
-        def format_key(state: State, key: GetItem.KeyValueType):
+        def format_key(state: State, key: GetItem.KeyValueType, size: int):
             match key:
                 case int():
                     fkey = (key,)
                 case tuple():
                     fkey = key
+                case slice(start=None, stop=None):
+                    fkey = tuple(range(0, size))
+                case slice(start=None, stop=stop):
+                    fkey = tuple(range(0, stop))
+                case slice(start=start, stop=None):
+                    fkey = tuple(range(start, size))
                 case slice(start=start, stop=stop):
                     fkey = tuple(range(start, stop))
 
             return state, fkey
 
-        state, row_key = format_key(state, self.key[0])
-        state, col_key = format_key(state, self.key[1])
+        n_rows, n_cols = child.shape
+        state, row_key = format_key(state, self.key[0], n_rows)
+        state, col_key = format_key(state, self.key[1], n_cols)
 
         return state, init_get_item_sparse_repr(
             child=child,
